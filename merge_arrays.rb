08c8502c29e31e130e9b509e975050aec069e7d8
Rio Goodman <rio@goodmanship.org>
2025-08-24 15:10:28 -0500

    Wip - py time

---
+++ merge_arrays.rb
@@ -0,0 +1,12 @@
+# @param {Integer[]} nums1
+# @param {Integer} m
+# @param {Integer[]} nums2
+# @param {Integer} n
+# @return {Void} Do not return anything, modify nums1 in-place instead.
+def merge(nums1, m, nums2, n)
+  i = 0
+  j = 0
+  while i < m - 1 && j < n - 1 do
+    if nums1[i] <
+  end
+end
